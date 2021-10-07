d3d0534e521c295469dcaf19c5d009881fc3298e
CASSIO645 <cantonio645@gmail.com>
2021-10-07 15:06:34 -0300

    criada a detail view

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "ToDo"
+app_name = "Todo"
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path("detalhes/<int:id>", views.detail, name="detail"),
+
 
 ]
--- todo/core/views.py
+++ todo/core/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ToDo
+from .forms import ToDoForms
 
 
 def index(request):
     all_todos = ToDo.objects.all()
-    return render(request, 'core/index.html', context={"all_todos": all_todos})
+    form = ToDoForms()
+    return render(request, 'core/index.html', context={"all_todos": all_todos, "form":form})
+
+
+def detail(request, id):
+    todo = get_object_or_404(ToDo, pk=id)
+    return render(request, "core/detail.html", context={"todo":todo})
