b07724863bf250bec254028b80c6858c3aa825d1
CASSIO645 <cantonio645@gmail.com>
2021-10-07 17:27:30 -0300

    criada as funcoes edit e delete

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -6,6 +6,6 @@ app_name = "Todo"
 urlpatterns = [
     path('', views.index, name="index"),
     path("detalhes/<int:id>", views.detail, name="detail"),
-
-
+    path("editar/<int:id>", views.edit, name="edit"),
+    path("delete/<int:id>", views.delete, name="delete"),
 ]
--- todo/core/views.py
+++ todo/core/views.py
@@ -18,3 +18,25 @@ def index(request):
 def detail(request, id):
     todo = get_object_or_404(ToDo, pk=id)
     return render(request, "core/detail.html", context={"todo":todo})
+
+
+def edit(request, id):
+    todo = get_object_or_404(ToDo, pk=id)
+    form = ToDoForms(instance=todo)
+
+    if request.method == "POST":
+        form = ToDoForms(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("Todo:index")
+        
+        return render(request, 'core/edit.html', context={"form":form, "todo":todo})
+        
+    return render(request, 'core/edit.html', context={"form":form, "todo":todo})
+
+
+def delete(request, id):
+    todo = get_object_or_404(ToDo, pk=id)
+    todo.delete()
+    return redirect('Todo:index')
+    
